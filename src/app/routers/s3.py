f068eb13c3fb67f9ca194632972f11f389ee3027
Amit Pandey <37514457+amitpandey-io@users.noreply.github.com>
2025-03-12 14:59:35 -0400

    added get method

--- src/app/routers/s3.py
+++ src/app/routers/s3.py
@@ -1,10 +1,21 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
+from fastapi.responses import JSONResponse
+from app import config
+from boto3 import client as boto3_client
+from botocore.exceptions import ClientError
+# import mimetypes
 
 router = APIRouter(prefix="/s3", tags=["s3"])
 
 ## Path params
-@router.get("/{bucket}/{path}")
+@router.get("/{bucket}/{path:path}")
 async def get_search(bucket:str, path: str):
-    pass
-    return {"need to perform s3 fetch here"}
-
+    try:
+        c = boto3_client('s3', aws_access_key_id=config.access_key, aws_secret_access_key=config.secret_key)
+        content = c.get_object(Bucket=bucket, Key=path)
+        return Response(content=content['Body'].read(), media_type=content['ContentType'])
+    except ClientError:
+        return JSONResponse(
+            status_code=404, content={"message": f"Not found or other error occured"}
+        )
+    
